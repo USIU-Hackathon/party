3e14a910b630fe9fce2b28262f740c4a2345108a
nellyk <nellykiboi@gmail.com>
2015-03-14 13:24:16 +0300

    api classes

---
+++ app/playlist/serializers.py
@@ -0,0 +1,22 @@
+__author__ = 'Nelly'
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from  models.playlist import Playlist
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username''email', 'group')
+
+
+class GroupSerializer (serializers.HyperlinkedModelSerializer):
+    class Meta:
+                model = Group
+                fields = ('url', 'name')
+
+class PlaylistSerializer (serializers.HyperlinkedModelSerializer):
+	class Meta:
+		        model = Playlist
+		        fields = ('name','user','created_at','updated_at')
--- app/playlist/views.py
+++ app/playlist/views.py
@@ -1,1 +1,30 @@
 # Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from playlist.serializers import UserSerializer, GroupSerializer, PlaylistSerializer
+
+class UserViewSet(viewsets.ModelViewSet)
+
+"""
+API endpoint that allows users to be viewed or date
+"""
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+"""
+API endpoint that allows users to be viewed or date
+"""
+
+class GroupViewSet(viewsets.ModelViewSet):
+		queryset = Group.objects.all()
+		serializer_class = GroupSerializer
+
+"""
+API endpoint that allows playlist to be viewed 
+"""
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
+
+
