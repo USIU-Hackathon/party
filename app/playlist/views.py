50ced73f965b01c531da272623ed56b170595f89
bernardndegwa <mrbernardndegwa@gmail.com>
2015-03-14 16:34:26 +0300

    Create view for SoundCloud search

--- app/playlist/views.py
+++ app/playlist/views.py
@@ -3,10 +3,26 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from playlist.models import Playlist,Tracks
 from playlist.serializers import UserSerializer, GroupSerializer, PlaylistSerializer,TracksSerializer
+import soundcloud
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+	
+class SoundCloudSearch():
+	#create a client object with app credentials
+	client = soundcloud.Client(client_id='ed485d39d5d8d1214fd3142ff2485c1b')
+	
+	#find all tracks from the Africas talking return message
+	tracks = client.get('/tracks', q={{to finish}}, license=)
+	
+class DjGetMessageFromAfricasTalking():
+	
+	
+	
+	
+		
+
 
 
 
